4f7699f863fc1548e242be98b46e4e54c584edba
drilonqe <drilonqerimi7@gmail.com>
2025-03-11 23:57:23 -0500

    Update BlockChainDriver.java

--- src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
+++ src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
@@ -1,5 +1,8 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 /**
  * The main driver for the block chain program.
  */
@@ -9,8 +12,34 @@ public class BlockChainDriver {
      * The main entry point for the program.
      * @param args the command-line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // TODO: fill me in!
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        if (args.length != 1) {
+            return; 
+        }
+    int initial = Integer.parseInt(args[0]);
+    BlockChain blockchain = new BlockChain(initial);
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println(blockchain.toString());
+
+    while(true) {
+        System.out.println("command ?");
+        String command = scanner.nextLine();
+
+        switch(command) {
+             case "help":
+                    System.out.println("Valid commands:");
+                    System.out.println("    mine: discovers the nonce for a given transaction");
+                    System.out.println("    append: appends a new block onto the end of the chain");
+                    System.out.println("    remove: removes the last block from the chain");
+                    System.out.println("    check: checks that the blockchain is valid");
+                    System.out.println("    report: reports the balances of Alice and Bob");
+                    System.out.println("    help: prints this list of commands");
+                    System.out.println("    quit: quits the program");
+                    break;
+            
+        }
+    }
+
     }  
 }
